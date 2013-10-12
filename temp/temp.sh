889fe3738f17f5ef4d5749dc3c699f0bfc8d0b8a
Ye Tian <yetianooo@gmail.com>
2013-10-11 21:30:35 -0600

    temp plot

--- temp/temp.sh
+++ temp/temp.sh
@@ -5,8 +5,8 @@ psbasemap $REG $SCA -Ba30f10:"Azimuth (degree)":/a0.0002f0.0001:"Amplitude": -X3
 awk '{print 0,0,$2,$1,1}' J39A_azi_dis_amp_binavg | psxy $REG $SCA -Svs0.07c/0.5c/0.05c -Ctemp.cpt -O >> temp1.ps
 
 REG=-R0/20/10/25
-SCA=-Ja10/15/1/1/1
-psbasemap $REG -JX10 -Ba30f10:"Azimuth (degree)":/a2f1:"Amplitude": -X3 -Y3 -P -K > temp2.ps
+SCA=-JX10
+psbasemap $REG $SCA -Ba2f1:"Azimuth (degree)":/a2f1:"Amplitude": -X3 -Y3 -P -K > temp2.ps
 awk '{print 10,15,$2,$1,1}' J39A_azi_dis_amp_binavg | psxy $REG $SCA -Sv0.07c/0.5c/0.05c -Ctemp.cpt -O >> temp2.ps
-
+#psxy J39A_azi_dis_amp_binavg $REG $SCA -Jt.3 -Gred -O >> temp2.ps
 
