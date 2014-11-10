b056b7ceedb3c0a7c292d80d2c13c7c895c48025
Ye Tian <tianye@spice.colorado.edu>
2014-11-10 11:38:10 -0700

    MyOMP.h hard linked

--- MyOMP.h
+++ MyOMP.h
@@ -1,1 +1,13 @@
-/home/tianye/code/MyLibs/include/MyOMP.h
+#ifndef MYOMP_H
+#define MYOMP_H
+
+#ifdef _OPENMP
+#include <omp.h>
+#else
+typedef int omp_int_t;
+inline omp_int_t omp_get_num_threads() { return 1; }
+inline omp_int_t omp_get_max_threads() { return 1; }
+inline omp_int_t omp_get_thread_num() { return 0; }
+#endif
+
+#endif
