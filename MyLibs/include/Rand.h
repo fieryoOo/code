c57b2a38debd27d4dc9a67691b12d8ea8b48ec34
Ye Tian <yetianooo@gmail.com>
2016-06-10 17:37:34 -0600

    Timer now measures both CPU and Wall time; new method Rand::d_uniformI

--- MyLibs/include/Rand.h
+++ MyLibs/include/Rand.h
@@ -8,16 +8,20 @@
 /* -------------------- the RNG class-------------------- */
 class Rand {
    std::default_random_engine generator1;
+   std::uniform_int_distribution<> d_uniformI;
    std::uniform_real_distribution<float> d_uniform;
    std::normal_distribution<float> d_normal;
 public:
-   Rand() /* add a true random number from std::random_device to the time seed to ensure thread safety */
+   Rand(const int ib = 0, const int ie = 100) 
+		// add a true random number from std::random_device to the time seed to ensure thread safety 
       : generator1( std::chrono::system_clock::now().time_since_epoch().count() + std::random_device{}() )
+      , d_uniformI(ib, ie)
       , d_uniform(0., 1.)
       , d_normal(0., 1.) {}
 
    //~Rand() {} // should not be defined!!!
 
+   inline float UniformI() { return d_uniformI(generator1); }
    inline float Uniform() { return d_uniform(generator1); }
    inline float Normal() { return d_normal(generator1); }
 
--- MyLibs/include/Timer.h
+++ MyLibs/include/Timer.h
@@ -4,23 +4,54 @@
 #include <ctime>
 #include <unordered_map>
 #include <iostream>
+#include <chrono>
 
 class Timer {
+	using WClock = std::conditional< std::chrono::high_resolution_clock::is_steady,
+												std::chrono::high_resolution_clock,
+												std::chrono::steady_clock >::type;	// the Wall clock
 public:
-	Timer(const std::string &name_p0 = "t0") { 
+	Timer() { UpdateT0(); }
+
+	void UpdateT0() { t0CPU = std::clock(); t0Wall = WClock::now(); }
+
+	double CPUSec(const std::string &name = "t0") {
+		return (std::clock() - t0CPU) / (double)CLOCKS_PER_SEC;
+	}
+
+	double WallSec(const std::string &name = "t0") {
+		return std::chrono::duration_cast<std::chrono::duration<double>>(WClock::now() - t0Wall).count();
+	}
+
+private:
+	std::clock_t t0CPU;
+	WClock::time_point t0Wall;
+};
+
+
+class Timer2 {
+	using WClock = std::conditional< std::chrono::high_resolution_clock::is_steady,
+												std::chrono::high_resolution_clock,
+												std::chrono::steady_clock >::type;	// the Wall clock
+public:
+	Timer2(const std::string &name_p0 = "t0") { 
 		AddTimePoint(name_p0);
 	}
 
 	void AddTimePoint(const std::string &name) {
-		timePoints.emplace(name, clock());	
+		timePoints.emplace(name, std::make_pair(std::clock(),WClock::now()));	
 	}
 
-	double SecondsElapsed(const std::string &name = "t0") {
-		return (clock() - timePoints[name]) / (double)CLOCKS_PER_SEC;
+	double CPUSec(const std::string &name = "t0") {
+		return (std::clock() - timePoints[name].first) / (double)CLOCKS_PER_SEC;
+	}
+
+	double WallSec(const std::string &name = "t0") {
+		return std::chrono::duration_cast<std::chrono::duration<double>>(WClock::now() - timePoints[name].second).count();
 	}
 
 private:
-	std::unordered_map<std::string, clock_t> timePoints;
+	std::unordered_map<std::string, std::pair<std::clock_t, WClock::time_point>> timePoints;
 };
 
 #endif
